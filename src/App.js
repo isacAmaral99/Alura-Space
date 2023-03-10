cb5bb247a0c587f277ed18ba2eb2338653749a5d
isacAmaral99 <isacamaralbueno@gmail.com>
2023-03-09 23:03:53 -0300

    incluindo componente de pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import PaginaInicial from "./paginas/PaginaInicial";
+import './styles/estilosGlobais.scss'
 
 
 export default function App() {
   return (
-    <h1> Alura Space</h1>
+    <PaginaInicial/>
   );
 }
 
